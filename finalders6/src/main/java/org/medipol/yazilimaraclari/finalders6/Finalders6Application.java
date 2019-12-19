9fa83b3d93b5860e748bd1ed816dd88e4a3cce70
bprlab <bprlab@10.200.144.21>
2019-12-19 21:12:14 +0300

    hello.

---
+++ finalders6/src/main/java/org/medipol/yazilimaraclari/finalders6/Finalders6Application.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.finalders6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalders6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalders6Application.class, args);
+	}
+
+}
---
+++ finalders6/src/test/java/org/medipol/yazilimaraclari/finalders6/Finalders6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.finalders6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalders6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
